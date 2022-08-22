c998cf1bf99fe7eb6f8eb56fd7c5cb8f60b8ed9e
SSI Digital <42675358+ssiedu@users.noreply.github.com>
2022-08-22 12:39:54 +0530

    variable/comment

---
+++ swap.py
@@ -0,0 +1,11 @@
+x=eval(input("Enter the value of x : "))
+y=eval(input("Enter the value of y : "))
+"""z= x; #temp=10
+x=y; #x=20
+y=z;"""
+'''x = x+y;
+y= x-y;
+x= x-y'''
+x,y = y,x
+print("value of x : ",x)  
+print("value of y : ",y)
