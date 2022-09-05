4c3fb2ddd10e6be48c04c56de157c3ab6d6cb54d
SSI Digital <42675358+ssiedu@users.noreply.github.com>
2022-09-05 12:42:10 +0530

    loop

---
+++ ifelif4.py
@@ -0,0 +1,13 @@
+num=int(input("Enter Any Number : "))
+if num==1:
+    print("One")
+elif num==2:
+    print("Two")
+elif num==3:
+    print("Three")
+elif num==4:
+    print("Four")
+elif num==5:
+    print("Five")
+else:
+    print("Invalid")
---
+++ while1.py
@@ -0,0 +1,5 @@
+s=int(input("Enter Start Limit :"))
+e=int(input("Enter end limit :"))
+while (s<=e):
+    print(s,end=" ")
+    s=s+2;
