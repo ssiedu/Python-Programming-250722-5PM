0d26a69c00acdf62c0f77ec129bdc7dc8b846974
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-10-01 15:51:26 +0530

    program

---
+++ empfile1.py
@@ -0,0 +1,8 @@
+f=open("Emp.txt","w")
+for i in range(3):
+    name=input("Enter the name :")
+    age = int(input("Enter age : "))
+    rec= name + " " + str(age)+ "\n"
+    f.write(rec)
+#f.write(str(age))
+f.close()
