d819ab6ab577a91902e39952838001729720c98e
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2022-10-10 19:54:52 +0530

    program

---
+++ fun1.py
@@ -0,0 +1,14 @@
+from funadd import *
+#import funadd
+#function definition
+def printHello():
+    print("Welcome to ssi digital")
+    print("This is python class")
+    print("Hello Everyone ")
+
+
+printHello() # function call
+
+#funadd.addition()
+addition()
+product()
---
+++ funadd.py
@@ -0,0 +1,23 @@
+def addition():
+    x=int(input("Enter the value of x : "))
+    y=int(input("Enter the value of y : "))
+    add=x+y
+    print("Sum of two numbers : ", add)
+
+
+def product():
+    x=int(input("Enter the value of x : "))
+    y=int(input("Enter the value of y : "))
+    product=x*y
+    print("Product of two numbers : ", product)
+
+
+
+
+
+
+
+
+'''for i in range(5):
+    addition()'''
+
